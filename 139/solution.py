264ab786ad819308d032aa2c616d534cd20f8811
robertlagrant <heroku@robertgrant.org>
2015-04-20 08:55:33 +0200

    139: fixed comments; reverted test data to original

--- 139/solution.py
+++ 139/solution.py
@@ -1,6 +1,7 @@
 import sys
 
-# Get list of month names and a count of months, inclusive, between two months
+# Get list of month names, inclusive, between two months
+# e.g. list_months('Jan', 'Mar') => ('Jan', 'Feb', 'Mar')
 def list_months(start_month, end_month):
     if start_month == end_month:
         return (start_month)
@@ -21,6 +22,7 @@ def list_months(start_month, end_month):
             list_of_months.append(month)
 
 # Get list of years, inclusive, as ints
+# E.g. list_years(2004, 2007) => (2004, 2005, 2006, 2007)
 def list_years(start_year, end_year):
     return range(start_year, end_year+1)
 
@@ -34,6 +36,7 @@ for line in lines:
     dates = line.split('; ')
     results = {}
     
+    # For each pair of dates, add in the correct months and years into the results dict
     for date in dates:
         start_string, end_string = date.split('-')
         start_year, start_month = int(start_string.split()[1]), start_string.split()[0]
@@ -55,13 +58,13 @@ for line in lines:
             for month in list_months(start, end):
                 results[year].add(month)
 
-    # Now we have a dict with years as keys and sets of months as values. Just add the months!
+    # Now we have a dict with years as keys and sets of months as values. Just sum the months!
     months_count = 0
     for year, months_set in results.items():
         months_count += len(months_set)
     
-    #years = months_count/12
-    years = (months_count + 12 // 2) // 12
+    years = months_count // 12
+    #years = (months_count + 12 // 2) // 12
     print(str(years))
     
 lines.close()
